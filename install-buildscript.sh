b442ecc45c4f03c04d467740d8bf03a8b4543b43
gnmmarechal <gnmpolicemata91@gmail.com>
2016-10-28 18:59:52 +0100

    Added description

--- install-buildscript.sh
+++ install-buildscript.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+echo "Downloading buildscr..."
 #This script downloads all required files for the initial installation using git. It's literally "git clone <repo>"
 #Git Repository
+echo "Setting variables..."
 gitrepo="https://github.com/gnmmarechal/3dsbuildscripts"
 gitdir="buildscr"
 
-
+echo "Cleaning..."
 #Pre clean up
-./clean
+rm -rf gitdir
 
+echo "Cloning repository..."
 #Instructions
 git clone --recursive "$gitrepo" "$gitdir"
 cd "$gitdir"
+echo "Setting permissions..."
 chmod +x *
 cd ..
-echo "Installed! cd to the directory to use the buildscripts!"
+echo "Done! cd to the directory to use the buildscripts!"
